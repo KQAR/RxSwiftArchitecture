4f86b77a1abe8657947fd83f70a1be1044e7963c
KQAR <31615748+KQAR@users.noreply.github.com>
2022-12-09 16:49:51 +0800

    Removed tracked files which shouldn't be tracked

--- Pods/Target Support Files/Pods-BaseView/Pods-BaseView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BaseView : NSObject
-@end
-@implementation PodsDummy_Pods_BaseView
-@end
--- Pods/Target Support Files/Pods-Mediator/Pods-Mediator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Mediator : NSObject
-@end
-@implementation PodsDummy_Pods_Mediator
-@end
