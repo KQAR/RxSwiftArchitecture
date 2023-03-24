e1cbbeb7ec09722b3a12c3c8bd62537a2a66cade
KQAR <31615748+KQAR@users.noreply.github.com>
2023-03-24 15:54:46 +0800

    add Array extension

---
+++ Extension/Array.swift
@@ -0,0 +1,31 @@
+//
+//  Array.swift
+//  Extension
+//
+//  Created by Jarvis on 2023/3/24.
+//
+
+import Foundation
+
+extension Collection {
+  /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+  subscript(safe index: Index) -> Element? {
+    if indices.contains(index) {
+      return self[index]
+    } else {
+      return nil
+    }
+  }
+}
+
+extension Array {
+  @discardableResult
+  mutating func safeRemove(at index: Index) -> Bool {
+    if indices.contains(index) {
+      remove(at: index)
+      return true
+    } else {
+      return false
+    }
+  }
+}
